f2c34e6280b16a7535c65a905c7e0141d447da1a
Lucas <lucasparente@id.uff.br>
2022-09-06 12:12:45 -0300

    Adicionando palavras reservadas e tokens

---
+++ lexico.py
@@ -0,0 +1,49 @@
+keywords = {
+  'class':'class',
+  'else': 'else',
+  'false': 'false',
+  'fi': 'fi',
+  'if': 'if',
+  'in': 'in',
+  'inherits': 'inherits',
+  'isvoid': 'isvoid',
+  'let': 'let',
+  'loop':'loop',
+  'pool':'poll',
+  'then':'then',
+  'while':'while',
+  'case':'case',
+  'esac':'esac',
+  'new':'new',
+  'of':'of',
+  'not':'not',
+  'true':'true',
+  'IO' : 'io',
+  'Object' : 'object',
+  'Int' : 'int',
+  'String' : 'string'
+
+}
+
+
+tokens = [
+  'ID',
+  'NUMERO',
+  'STRING',
+  'MAIS',
+  'MENOS',
+  'MULTIPLICACAO',
+  'DIVISAO',
+  'IGUAL',
+  'MENORIGUAL',
+  'MENOR',
+  'PONTO',
+  'DOISPONTOS',
+  'PONTOEVIRGULA',
+  'VIRGULA',
+  'ATRIBUICAO',
+  'ABREPARENTESES',
+  'FECHAPARENTESES',
+  'ABRECHAVES',
+  'FECHACHAVES'
+]
