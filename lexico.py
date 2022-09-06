d8706141778f2a09b44b557a3d8d6590f977ce6a
Lucas <lucasparente@id.uff.br>
2022-09-06 17:15:41 -0300

    Adicionando lex.py e definindo tokens

--- lexico.py
+++ lexico.py
@@ -17,12 +17,7 @@ keywords = {
   'new':'new',
   'of':'of',
   'not':'not',
-  'true':'true',
-  'IO' : 'io',
-  'Object' : 'object',
-  'Int' : 'int',
-  'String' : 'string'
-
+  'true':'true'
 }
 
 
@@ -46,4 +41,47 @@ tokens = [
   'FECHAPARENTESES',
   'ABRECHAVES',
   'FECHACHAVES'
-]
+] + list(keywords.values())
+
+t_MAIS = r'\+'
+t_MENOS = r'\-'
+t_MULTIPLICACAO = r'\*'
+t_DIVISAO = r'\/'
+t_IGUAL = r'\='
+t_MENORIGUAL = r'\<\='
+t_MENOR = r'\<'
+t_PONTO = r'\.'
+t_DOISPONTOS = r'\:'
+t_PONTOEVIRGULA = r'\;'
+t_VIRGULA = r'\,'
+t_ATRIBUICAO = r'\<\-'
+t_ABREPARENTESES = r'\('
+t_FECHAPARENTESES = r'\)'
+t_ABRECHAVES = r'\{'
+t_FECHACHAVES = r'\}'
+
+t_ignore = ' \t'
+
+def t_ID(t):
+	r'[a-zA-z_][a-zA-Z_0-9]*'
+	if t.value in keywords:
+	    t.type = t.value.upper()
+	else:
+	    t.type = 'ID'
+	return t
+
+def t_NUMERO(t):
+	r'\d+'
+	return t
+
+def t_STRING(t):
+    r'\"[\w_\W]*\"'
+    return t
+
+def t_NOVALINHA(t):
+	r'\n+'
+	t.lexer.lineno += len(t.value)
+
+def t_error(t):
+    print(f'Caractere ilegal {t.value[0]!r}')
+    t.lexer.skip(1)
